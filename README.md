87486896dea0796190d4c728a17316889b28acad
Christian Hammond <christian@beanbaginc.com>
2015-07-06 20:54:07 -0700

    Initial version of itermlauncher.
    This is a handy little program that will launch iTerm2 profiles through
    a retro console menu. It's most useful in the iTerm2 hotkey window, for
    sessions you want to keep handy but mostly out-of-the-way.

---
+++ README.md
@@ -0,0 +1,97 @@
+iTermLauncher for iTerm2
+========================
+
+iTermLauncher is a handy console-based menu for quickly launching new
+terminals within the same window using iTerm2
+
+This is intended for use with the iTerm2 Hotkey window, for launching
+additional terminals within that window. I personally use it to keep all my
+development servers or SSH sessions in reach, all within the hotkey window.
+
+Once launched, the menu will remain running until you hit Control-C or Q.
+It will scan all the profiles you have configured (optionally filtering by
+tags), and let you choose one to launch within the window.
+
+
+Installing iTermLauncher
+------------------------
+
+Installation is simple. Just make sure you have node.js installed:
+
+    $ npm install itermlauncher
+
+To run it, just run:
+
+    $ itermlauncher
+
+
+Configuring iTermLauncher
+-------------------------
+
+Configuration is completely optional. iTermLauncher will find all your
+profiles and do the right thing.
+
+There are a few configuration options, though. You can set the foreground/background colors, the width of the menu, an exported iTerm2 plist file, and the
+list of tags required for a profile to appear.
+
+If you want to change things, create a JSON file in
+`~/.config/itermlauncher/config.json`. This can look something like:
+
+    {
+		"bg": "black",
+		"fg": "yellow",
+		"menuWidth": 50,
+		"requiredTags": [
+		    "Dev Servers"
+		],
+	    "plistFilename": "/path/to/com.googlecode.iterm2.plist"
+	}
+
+The one you most likely want to set is `requiredTags`. Any profile appearing
+in the menu must have all of the listed tags.
+
+You can also specify a custom configuration file as an argument when running
+`itermlauncher`:
+
+    $ itermlauncher my-config.json
+
+
+Configuring iTerm2
+------------------
+
+You'll want to make this easy and basically automatic. I suggest hooking this
+up to the hotkey window. Just follow these steps:
+
+1. Go into the iTerm2 profile editor and select the profile for your
+   Hotkey window (or create it if it doesn't exist).
+2. In the General tab for the profile, set Command to: `itermlauncher`
+3. In the Keys settings, under Hotkey, make sure both checkboxes are checked,
+   set your desired hotkey, and make sure it's mapped to the profile you
+   just configured.
+4. Press the hotkey. Tada!
+
+
+License
+-------
+
+iTermLauncher is MIT-licensed.
+
+Copyright (c) 2015  Christian Hammond
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ bin/itermlauncher.js
@@ -0,0 +1,145 @@
+var applescript = require('applescript'),
+    childProcess = require('child_process'),
+    fs = require('fs'),
+    Menu = require('terminal-menu'),
+    path = require('path-extra'),
+    plist = require('plist');
+
+
+function getIterm2Config(config) {
+    var data;
+
+    if (config.plistFilename) {
+        data = fs.readFileSync(config.plistFilename, 'utf8');
+    } else {
+        data = childProcess.execSync(
+            'defaults export com.googlecode.iterm2 -').toString();
+    }
+
+    return plist.parse(data);
+}
+
+function buildEntries(config) {
+    var plistData = getIterm2Config(config),
+        requiredTags = config.requiredTags || [],
+        itermBookmarks = plistData['New Bookmarks'],
+        entries = [];
+
+    itermBookmarks.forEach(function(bookmark) {
+        var valid = true,
+            tags = {};
+
+        bookmark.Tags.forEach(function(tag) {
+            tags[tag] = true;
+        });
+
+        valid = requiredTags.every(function(tag) {
+            return tags.hasOwnProperty(tag);
+        });
+
+        if (valid) {
+            entries.push(bookmark.Name);
+        }
+    });
+
+    return entries
+}
+
+function buildMenu(config, menuEntries) {
+    var NUM_DEFAULT_ITEMS = 5;
+
+    function onError(err, rtn) {
+        menu.close();
+
+        console.log('Failed to launch the session: %s', err);
+        process.exit(1);
+    };
+
+    var menuWidth = config.menuWidth || 50,
+        menuHeight = NUM_DEFAULT_ITEMS + menuEntries.length,
+        horizPadding = Math.floor((process.stdout.columns - menuWidth) / 2.0),
+        vertPadding = Math.floor((process.stdout.rows - menuHeight) / 2.0)
+
+    var menu = Menu({
+        width: menuWidth,
+        x: 0,
+        y: 0,
+        bg: config.bg,
+        fg: config.fg,
+        padding: {
+            left: horizPadding,
+            right: horizPadding,
+            top: vertPadding,
+            bottom: vertPadding
+        }
+    });
+
+    menu.reset();
+    menu.write('Launch a Terminal\n');
+    menu.write('=================\n\n');
+
+    menu.add('Default Terminal', function() {
+        launchProfile('Default', onError);
+    });
+
+    menu.write('\n');
+
+    menuEntries.forEach(function(entry) {
+        menu.add(entry, function() {
+            launchProfile(entry, onError);
+        });
+    });
+
+    menu.on('close', function() {
+        process.stdin.setRawMode(false);
+        process.stdin.end();
+    });
+
+    process.stdin.pipe(menu.createStream()).pipe(process.stdout);
+    process.stdin.setRawMode(true);
+}
+
+function launchProfile(name, onError) {
+    var script = [
+        'tell application "iTerm"',
+        '    tell the current terminal',
+        '        launch session "' + name + '"',
+        '    end tell',
+        'end tell'
+    ].join('\n');
+
+    applescript.execString(script, function(err, rtn) {
+        if (err) {
+            onError(err, rtn);
+        }
+    });
+}
+
+function readConfig(filename) {
+    if (fs.existsSync(filename)) {
+        try {
+            return require(filename);
+        } catch (e) {
+            console.log('Failure when parsing configuration file "%s": %s',
+                        filename, e);
+            process.exit(1);
+        }
+    }
+
+    return {};
+}
+
+function main() {
+    if (process.argv.length === 3) {
+        configFilename = process.argv[2];
+    } else {
+        configFilename = path.join(path.homedir(), '.config',
+                                   'itermlauncher', 'config.json');
+    }
+
+    config = readConfig(configFilename);
+
+    buildMenu(config, buildEntries(config));
+}
+
+main();
